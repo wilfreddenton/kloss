a52a24ee81a54b604f699affd877404776c9c86d
Wilfred Denton <dentonw3@gmail.com>
2016-02-06 15:27:46 -0500

    navigation is now usable but still needs its design worked on

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,20 +13,27 @@
 })(jQuery);
 
 // how about some real Javascript Ghost?
+// action button
 (function() {
   var opened = false;
   var hideNav = function(e) {
     if (!opened) return;
-    console.log('hey');
     opened = false;
     var ink = document.getElementById('ink');
+    var navItems = document.getElementsByClassName('nav-item');
     ink.classList.remove('animate-ripple');
+    Array.prototype.forEach.call(navItems, function(navItem, i) {
+      setTimeout(function() {
+        navItem.classList.remove('show');
+      }, 100 * i)
+    });
   }
   var showNav = function(e) {
     if (opened) return;
     opened = true;
     var ele = document.getElementById('action-button');
     var ink = document.getElementById('ink');
+    var navItems = document.getElementsByClassName('nav-item');
     var d = Math.max(window.innerWidth, window.innerHeight);
     var right = (-(d / 2) + ele.offsetWidth / 2 + 25).toString() + 'px';
     var bottom = (-(d / 2) + ele.offsetHeight / 2 + 15).toString() + 'px';
@@ -38,10 +45,28 @@
       ink.style.right = right;
       ink.style.bottom = bottom;
       ink.classList.add('animate-ripple');
+      Array.prototype.forEach.call(navItems, function(navItem, i) {
+        setTimeout(function() {
+          navItem.classList.add('show');
+        }, 100 * (navItems.length - i));
+      });
     }, 23);
   }
   var ab = document.getElementById('action-button').childNodes[1];
   ab.addEventListener('click', showNav);
   ab.addEventListener('mouseover', showNav);
   ink.addEventListener('click', hideNav);
+})();
+
+// adding ellipse to the index excerpts
+(function() {
+  var excerpts = document.getElementsByClassName('post-excerpt');
+  if (excerpts.length > 0) {
+    Array.prototype.forEach.call(excerpts, function(excerpt) {
+      var paras = excerpt.getElementsByTagName('p');
+      var p = paras[paras.length - 2];
+      if (p.childNodes.length === 1 && p.childNodes[0].nodeName === '#text')
+        p.innerHTML += '...';
+    });
+  }
 })();
